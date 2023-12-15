81de3d0b3eba16a347a3e602ab8a691f948e4e5c
Apple <apple@192.168.1.2>
2023-12-15 22:28:17 +0530

    Commited Successfull

--- src/components/About/ExploreMore.js
+++ src/components/About/ExploreMore.js
@@ -1,20 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import eswar from '../../assets/images/IMG_20221211_112000_863.jpg'
+import Navbar from '../Header/header'
+import Footer from '../Footer/footer'
 const ExploreMore = () => {
+    useEffect(()=>{
+        window.scrollTo(0,0)
+    },[])
   return (
     <>
+    
     <div className=''>
-        <div className='flex justify-center items-center'>
+        <h2 className='flex justify-center items-center mt-4 text-[35px]'>About Eswar Nadendla?</h2>
+        <div className='flex justify-center items-center '>
             <div className=''>
                
-                <img className='w-[250px] h-[250px] rounded-full mt-8' src={eswar}/>
+                <img className='shadow-2xl w-[300px] h-[300px] rounded-full mt-8' src={eswar}/>
             </div>
         </div>
         <div className='flex justify-center mt-6'>
-            <h2 className='text-[34px] font-semibold text-headingColor'>Eswar Nadendla</h2>
+            {/* <h2 className='text-[34px] font-semibold text-headingColor'>Eswar Nadendla</h2> */}
            
         </div>
-        <div className='mx-auto'>
+        <div className='mx-6 md:mx-32 lg:mx-56 mt-4 leading-7'>
         <p>I am Eswar Nadendla, a highly skilled web and app developer with over 1 year of professional experience. My expertise lies in HTML, CSS, JavaScript, and React for web development. Additionally, I am proficient in React Native for cross-platform app development, leveraging Node.js and MongoDB to ensure robust backend support. My programming knowledge extends to Java and Python, allowing me to tackle a diverse range of challenges.
 
 My commitment to excellence is evident in my approach to crafting innovative solutions. With a strong passion for coding and creativity, I bring a unique perspective to every project, ensuring not only efficient results but also impactful and user-friendly experiences.
@@ -27,6 +34,112 @@ In terms of education, I pursued my Bachelor's degree in Computer Science and En
 
 </p>
         </div>
+<div className='bg-gray-50 py-10 mt-8'>
+
+<div className='flex justify-center mt-6'>
+        <h2 className='text-[34px] font-semibold text-headingColor'>Education</h2>
+        </div>
+
+
+        <div className='mx-8 md:mx-56 mt-12'>
+
+        <div class="space-y-8 relative before:absolute before:inset-0 ml-5 before:-translate-x-px md:before:mx-auto md:before:translate-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-slate-300 before:to-transparent">
+
+
+<div class="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
+ 
+<div class="flex hidden items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2 mx-auto my-auto">
+        <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="12" height="10">
+            <path fill-rule="nonzero" d="M10.422 1.257 4.655 7.025 2.553 4.923A.916.916 0 0 0 1.257 6.22l2.75 2.75a.916.916 0 0 0 1.296 0l6.415-6.416a.916.916 0 0 0-1.296-1.296Z" />
+        </svg>
+    </div>
+   
+    <div class="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-slate-200 shadow">
+        <div class="lg:flex items-center justify-between space-x-2 mb-1">
+            <div class="font-bold text-slate-900">B.Tech</div>
+            <time class="font-caveat font-medium text-[#ea580c]">05/2023</time>
+        </div>
+
+        <div class="text-slate-500 overflow-hidden">Completed my B.Tech in the stream of Computer Science and Engineering from Sri Venkateswara College of Engineering,Tirupati</div>
+    </div>
+</div>
+
+
+<div class="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
+
+    <div class="flex hidden items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
+        <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="12" height="10">
+            <path fill-rule="nonzero" d="M10.422 1.257 4.655 7.025 2.553 4.923A.916.916 0 0 0 1.257 6.22l2.75 2.75a.916.916 0 0 0 1.296 0l6.415-6.416a.916.916 0 0 0-1.296-1.296Z" />
+        </svg>
+    </div>
+ 
+    <div class="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-slate-200 shadow">
+        <div class="lg:flex items-center justify-between space-x-2 mb-1">
+            <div class="font-bold text-slate-900">Web Development</div>
+            <time class="font-caveat font-medium text-[#ea580c]">04/2021</time>
+        </div>
+        <div class="text-slate-500">Started web development career</div>
+        <div class="text-slate-500 overflow-hidden">Started by watching Youtube Videos and developed projects.</div>
+    </div>
+</div>
+
+
+<div class="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
+
+    <div class="flex hidden items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
+        <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="12" height="10">
+            <path fill-rule="nonzero" d="M10.422 1.257 4.655 7.025 2.553 4.923A.916.916 0 0 0 1.257 6.22l2.75 2.75a.916.916 0 0 0 1.296 0l6.415-6.416a.916.916 0 0 0-1.296-1.296Z" />
+        </svg>
+    </div>
+  
+    <div class="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-slate-200 shadow">
+        <div class="lg:flex items-center justify-between space-x-2 mb-1">
+            <div class="font-bold text-slate-900">Intermediate</div>
+            <time class="font-caveat font-medium text-[#ea580c]">04/2019</time>
+        </div>
+        <div class="text-slate-500">Completed my 12th standard!</div>
+        <div class="text-slate-500 overflow-hidden">Completed intermediate from Mangal Junior College,Petamitta</div>
+    </div>
+</div>
+
+
+<div class="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
+    
+    <div class="flex hidden items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
+        <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="12" height="10">
+            <path fill-rule="nonzero" d="M10.422 1.257 4.655 7.025 2.553 4.923A.916.916 0 0 0 1.257 6.22l2.75 2.75a.916.916 0 0 0 1.296 0l6.415-6.416a.916.916 0 0 0-1.296-1.296Z" />
+        </svg>
+    </div>
+   
+    <div class="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-slate-200 shadow">
+        <div class="lg:flex items-center justify-between space-x-2 mb-1">
+            <div class="font-bold text-slate-900">10th Standard</div>
+            <time class="font-caveat font-medium text-[#ea580c]">04/2017</time>
+        </div>
+        <div class="text-slate-500 overflow-hidden">Completed my 10th Standard from Ushodaya High School,Pakala</div>
+    </div>
+</div>
+
+
+
+
+</div>
+
+
+        </div>
+
+        <div className='mb-12'>
+
+        </div>
+</div>
+        <div class="bg-headingColor p-6 text-center dark:bg-headingColor">
+    <span className='text-white'>© 2023 Copyright:</span>
+    <a
+      class="font-semibold text-white"
+      href="#"
+      >Eswar Nadendla</a
+    >
+  </div>
     </div>
     </>
   )
